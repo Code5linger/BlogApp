84036d9b78a445a4212c08bdb66c6e2d7ea1d07e
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2026-04-22 14:49:59 +0600

    !

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Render + Docker!'));
+app.get('/', (req, res) => res.send('Hello from Render + Docker + CI/CD!'));
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
