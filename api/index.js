81dd546f3d4910d462fb7552615dac5dcdd0dacd
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2026-04-22 14:48:24 +0600

    !

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Vercel-CI-CD!'));
+app.get('/', (req, res) => res.send('Hello from Render + Docker!'));
 
-module.exports = app;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
