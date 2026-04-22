1a6f56c107b67df88174b81143743acdeaf97533
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2026-04-22 14:20:14 +0600

    !

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello from Vercel!'));
+app.get('/', (req, res) => res.send('Hello from Vercel-CI-CD!'));
 
 module.exports = app;
